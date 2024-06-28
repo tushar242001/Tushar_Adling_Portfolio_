320d8eefc97d485fbf30a327244e5f0f94ff6dea
tushar242001 <tusharadling24@gmail.com>
2024-06-28 14:49:05 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+function firstpageAnim()
+{
+    var tl = gsap.timeline();
+
+    tl.from("#nav",{
+        y : '-10',
+        opacity : 0 ,
+        duration: 2,
+        ease: Expo.easeInOut
+    })
+    .to(".boundingelem",{
+        y : '0',
+        duration: 2,
+        stagger: .2,
+        ease: Expo.easeInOut,
+        delay: -1
+       
+    })
+    .from("#herofooter",{
+        y : '-10',
+        opacity : 0 ,
+        duration: 1.5,
+        delay: -1,
+        ease: Expo.easeInOut
+    })
+
+}
+
+
+
+function circlemousefollower()
+{
+    window.addEventListener("mousemove",function(dets){
+        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px,${dets.clientY}px)`;
+    })
+
+}
+circlemousefollower();
+firstpageAnim();
